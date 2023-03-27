4051774b13a0e3a617db1a5fc9ef6750de29236d
PavelZhikh <jiharpv@gmail.com>
2023-03-27 22:54:59 +0300

    add user

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from api import app
 from api.models.quote import QuoteModel
 from api.handlers import author
 from api.handlers import quote
+from api.handlers import user
 from config import Config
 
 
